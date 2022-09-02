dde8b770b2fae8b6a1aa55cdd6eecdf022476a3b
Jesper Kihlberg <jesper.kihlberg@gmail.com>
2022-09-02 13:23:04 +0200

    Changed types

--- apps/app1/webpack.config.js
+++ apps/app1/webpack.config.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const deps = require("./package.json").dependencies;
 module.exports = {
   output: {
-    publicPath: "http://localhost:3033/",
+    publicPath: "http://localhost:3111/",
   },
 
   resolve: {
@@ -12,7 +12,7 @@ module.exports = {
   },
 
   devServer: {
-    port: 3033,
+    port: 3111,
     historyApiFallback: true,
   },
 
